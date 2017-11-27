8cef512dfc9029ac91d75cc9080d13672651d018
Patrick Werner <pwerner2@gmail.com>
2017-11-27 12:45:55 -0500

    rewritten. hooray for overthinking

--- parser.php
+++ parser.php
@@ -1,57 +1,33 @@
 <?php
-$request_url = "https://www.myrtlebeach.com/sitemap_index.xml";
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, $request_url);
-curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 30);
-curl_setopt($curl, CURLOPT_TIMEOUT, 3);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://www.myrtlebeach.com/sitemap_index.xml');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$xml = curl_exec($ch);
+curl_close ($ch);
 
-$response = curl_exec($curl);
-curl_close($curl);
+$xml = simplexml_load_string($xml);
+foreach($xml->sitemap as $item){
+    if (strpos($item->loc, 'wpl-') !== false) {
 
-$xml = simplexml_load_string($response);
-$json = json_encode($xml);
-$arr = json_decode($json,true);
+        $sub_ch = curl_init();
+        curl_setopt($sub_ch, CURLOPT_URL, $item->loc);
+        curl_setopt($sub_ch, CURLOPT_RETURNTRANSFER, 1);
+        $sub_xml = curl_exec($sub_ch);
+        curl_close ($sub_ch);
 
-$top_lvl = array();
+        $sub_xml = simplexml_load_string($sub_xml);
+        foreach($sub_xml as $sub_item){
+            echo $sub_item->loc . "<Br />\n";
 
-foreach($arr as $key=>$value) {
-    foreach($value as $key1=>$value1) {
-        $top_lvl[$key][$key1] = $value1;
-        $urls = $value1;
+            $single_ch = curl_init();
+            curl_setopt($single_ch, CURLOPT_URL, $sub_item->loc);
+            curl_setopt($single_ch, CURLOPT_FRESH_CONNECT, true);
+            curl_setopt($single_ch, CURLOPT_TIMEOUT_MS, 1);
+            curl_exec($single_ch);
+            curl_close($single_ch);
+
+            break; // REMOVE IN PRODUCTION
+        }
     }
 }
-
-$toplvl_arr = print_r($top_lvl);
-
-echo "<pre>";$toplvl_arr;echo "</pre>";
-
-
-$lookfor='/wpl-';
-
-foreach ($urls as $url){
-    if(substr($url->getAttribute('loc'), 0, strlen($lookfor)) == $lookfor) {
-        echo "<br> $url";
-        echo "<hr><br>";
-    }
-}
-
-//second version - meh
-//$ch = curl_init();
-//curl_setopt($ch, CURLOPT_URL, 'https://www.myrtlebeach.com/sitemap_index.xml');
-//curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-//$xml = curl_exec ($ch);
-//curl_close ($ch);
-
-//if (@simplexml_load_string($xml)) {
-//$fp = fopen('feed.xml', 'w');
-//fwrite($fp, $xml);
-//echo $xml;
-//fclose($fp);
-//}
-
-//$xml = new SimpleXMLElement($data);
-//foreach ($xml->url as $url_list) {
-//$url = $url_list->loc;
-//echo $url;
-//}
+?>
